dfe25c7752b95b58b7ff0c8f9f258a0993ee3fed
Cleiton Pereira <cleiton.spereira@live.com>
2021-05-01 08:44:07 -0300

    🔧 configra banco de dados em tempo de execução

--- lib/rocketpay/repo.ex
+++ lib/rocketpay/repo.ex
@@ -2,4 +2,11 @@ defmodule Rocketpay.Repo do
   use Ecto.Repo,
     otp_app: :rocketpay,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    case System.get_env("DB_URL") do
+      nil -> {:ok, config}
+      url -> {:ok, Keyword.put(config, :url, url)}
+    end
+  end
 end
